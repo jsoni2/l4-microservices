ed6e8f58a62adf1427f99370c52b51638f803f9c
Janak Soni <janaksoni91@gmail.com>
2021-05-24 21:00:54 -0500

    Naming

--- items/src/main/java/com/udacity/item/ItemApplication.java
+++ items/src/main/java/com/udacity/item/ItemApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class MicroservicesApplication {
+public class ItemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroservicesApplication.class, args);
+        SpringApplication.run(ItemApplication.class, args);
     }
 
 }
--- items/src/test/java/com/udacity/item/ItemApplicationTests.java
+++ items/src/test/java/com/udacity/item/ItemApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MicroservicesApplicationTests {
+public class ItemApplicationTests {
 
     @Test
     public void contextLoads() {
