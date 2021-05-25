e1d1f8825760ef95292e6387629c83726c823dbd
Janak Soni <janaksoni91@gmail.com>
2021-05-24 21:00:18 -0500

    Naming

--- dogs/src/main/java/com/udacity/dog/DogApplication.java
+++ dogs/src/main/java/com/udacity/dog/DogApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class DogsMicroservicesApplication {
+public class DogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DogsMicroservicesApplication.class, args);
+        SpringApplication.run(DogApplication.class, args);
     }
 
 }
--- dogs/src/test/java/com/udacity/dog/DogApplicationTests.java
+++ dogs/src/test/java/com/udacity/dog/DogApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DogsMicroservicesApplicationTests {
+public class DogApplicationTests {
 
     @Test
     public void contextLoads() {
